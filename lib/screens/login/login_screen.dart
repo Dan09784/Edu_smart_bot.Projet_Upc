c5fa22d4764946b214207d2906f2a5d04f0fc31b
Dan09784 <Dan09784@icloud.com>
2026-03-06 06:51:54 +0100

    Refactor code structure for improved readability and maintainability

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -86,16 +86,27 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(24),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "EduSmartBot",
-              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+      body: Stack(
+        children: [
+          // background image
+          Positioned.fill(
+            child: Image.asset(
+              'lib/assets/lottie/InscripBack.JPG',
+              fit: BoxFit.cover,
             ),
-            const SizedBox(height: 30),
+          ),
+
+          // form content
+          Padding(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "EduSmartBot",
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 30),
 
             TextField(
               controller: matriculeController,
@@ -134,6 +145,8 @@ class _LoginScreenState extends State<LoginScreen> {
           ],
         ),
       ),
+    ],
+  ),
     );
   }
 }
